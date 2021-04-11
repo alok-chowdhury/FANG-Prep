5d0286c2b14841ca0ad48cc324a33cbaf92b7ea2
Alok Chowdhury <alokc720@gmail.com>
2021-04-11 12:46:42 +0530

    Trying to cook something delicious :)

---
+++ src/letstrysomething/Helloworld.java
@@ -0,0 +1,11 @@
+package letstrysomething;
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi I am here just to demonstrate that I am java in action");
+
+	}
+
+}
